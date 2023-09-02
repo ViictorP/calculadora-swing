79da44fd0458ee7e347c40b141659249de376f0c
Victor Pimentel <victorpimentelw@gmail.com>
2023-09-01 23:31:21 -0300

    Padrão observer implementado.

--- src/main/java/com/victor/calc/modelo/Memoria.java
+++ src/main/java/com/victor/calc/modelo/Memoria.java
@@ -1,5 +1,37 @@
 package com.victor.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
 
+    private static final Memoria instancia = new Memoria();
+
+    private final List<MemoriaObservador> observadores = new ArrayList<>();
+
+    private String textoAtual = "";
+
+    private Memoria() {
+    }
+
+    public static Memoria getInstance() {
+        return instancia;
+    }
+
+    public void adicionarObservador(MemoriaObservador observador) {
+        observadores.add(observador);
+    }
+
+    public String getTextoAtual() {
+        return textoAtual.isEmpty() ? "0" : textoAtual;
+    }
+
+    public void processarComando(String valor) {
+        if ("AC".equals(valor)) {
+            textoAtual = "";
+        } else {
+            textoAtual += valor;
+        }
+        observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
 }
--- src/main/java/com/victor/calc/visao/Display.java
+++ src/main/java/com/victor/calc/visao/Display.java
@@ -1,19 +1,29 @@
 package com.victor.calc.visao;
 
+import com.victor.calc.modelo.Memoria;
+import com.victor.calc.modelo.MemoriaObservador;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class Display extends JPanel {
+public class Display extends JPanel implements MemoriaObservador {
 
     private final JLabel label;
     public Display() {
+        Memoria.getInstance().adicionarObservador(this);
+
         setBackground(new Color(46, 49, 50));
-        label = new JLabel("123,123");
+        label = new JLabel(Memoria.getInstance().getTextoAtual());
         label.setForeground(Color.WHITE);
         label.setFont(new Font("courier", Font.PLAIN, 30));
 
         setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 20));
 
         add(label);
+    }
+
+    @Override
+    public void valorAlterado(String novoValor) {
+        label.setText(novoValor);
     }
 }
